ccdc36f8939f15cb80b5edd599fd05bff13bec86
Bhoomika <bhoomika.jalandhra@gmail.com>
2026-05-17 13:29:57 +0530

    Variables

---
+++ datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; //treat all JS code as newer version
+
+console.log(3+3); console.log("Bhoomika") //avoid writing like this
+
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof 1)
---
+++ variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "bhoomika@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState
+
+//accountId = 2 (not allowed)
+accountEmail = "a@gmail.com"
+accountPassword = "1"
+
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountState)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
