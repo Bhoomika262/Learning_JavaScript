a728328a64e4f91b37249888e04c9f905180c964
Bhoomika <bhoomika.jalandhra@gmail.com>
2026-05-19 13:53:24 +0530

    Comparison of datatypes

---
+++ datatypes_comparison.js
@@ -0,0 +1,25 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2">1)
+console.log("02">1);
+
+//In the above case we got correct output but it is not always true
+//For comparison datatypes must be same
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+console.log(null==undefined);
+
+//=== -> strict check
+console.log("2"===2)
