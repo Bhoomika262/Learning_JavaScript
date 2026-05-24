d87e74fcf5b70bba63bbc526c5dd36563404d1fb
Bhoomika <bhoomika.jalandhra@gmail.com>
2026-05-24 10:23:52 +0530

    Numbers and maths

---
+++ num_maths.js
@@ -0,0 +1,38 @@
+const score=400
+console.log(score)
+
+const score1=new Number(100)
+console.log(score1)
+
+console.log(score1.toString())
+console.log(score1)
+console.log(typeof score1)
+console.log(typeof score1.toString())
+console.log(score1.toString().length)
+console.log(score1.toFixed(3))
+
+const other=23.8841
+console.log(other.toPrecision(2))
+console.log(other.toPrecision(3))
+
+const a=1000000
+console.log(a.toLocaleString())
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.1))
+console.log(Math.floor(4.8))
+
+console.log(Math.min(4,2,8,1))
+console.log(Math.max(4,2,8,1))
+
+console.log(Math.random())
+
+console.log((Math.random()*10)+1)
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
