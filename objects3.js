f0b4c631b84b165222369dbc1eeadb6f5a2fd413
Bhoomika <bhoomika.jalandhra@gmail.com>
2026-05-28 11:39:36 +0530

    Wrapping up objects in JS

---
+++ objects3.js
@@ -0,0 +1,21 @@
+const course={
+    name:"JS",
+    price:"500",
+    teacher:"Hitesh"
+}
+
+const{teacher}=course
+
+console.log(teacher) //no need to write course.teacher
+
+//destrcuturing of objects
+const navbar=({company})=>{
+
+}
+navbar(company="Bhoomika")
+
+{
+    name:"Bhoomika",
+    course:"JS",
+    price:"Free"
+}
