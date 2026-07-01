4a8fc15cbfc13089d230bc003c58fc7909591498
Bhoomika Jalandhra <bhoomika.jalandhra@gmail.com>
2026-07-01 12:54:21 +0530

    Understanding control flow

---
+++ control_flow.js
@@ -0,0 +1,14 @@
+val1 = 5 ?? 10
+console.log(val1)
+
+val2 = null ?? 10
+console.log(val2)
+
+val3 = undefined ?? 20
+console.log(val3)
+
+val4 = null ?? 10 ?? 20
+console.log(val4)
+
+//ternary operator
+//condition?true:false  
